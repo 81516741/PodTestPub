24d062e72be5b91e013a5a44faa7e2c48472e3e4
令达 <lingda@lingdadeMacBook-Pro.local>
2018-03-23 11:35:21 +0800

    版本升级维护

--- pod_update.sh
+++ pod_update.sh
@@ -6,12 +6,12 @@ target=$PWD
 target=${target##*/}
 echo 🔥执行:fastlane ManagerLib tag:$tag target:$target
 fastlane ManagerLib tag:$tag target:$target
-echo 请输入注册的邮箱
-read email
-echo 请输入注册的描述
-read des
-echo 🔥开始注册
-pod trunk register $email  $des  --verbose
+#echo 请输入注册的邮箱
+#read email
+#echo 请输入注册的描述
+#read des
+#echo 🔥开始注册
+#pod trunk register $email  $des  --verbose
 echo 执行提交...
 pod trunk push  $target.podspec  --allow-warnings
 
